b8c723a6117d889f4825446228bf62ce9c86fba6
Fredi Kats <fredikats@outlook.com>
2020-11-14 22:39:15 +0300

    feat: add queame commands

---
+++ Source/Quewer.BotClient/Commands/QueamCommands/AddQueamQueserCommand.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using FluentResults;
+using Tef.BotFramework.Abstractions;
+using Tef.BotFramework.Core;
+
+namespace Quewer.BotClient.Commands.QueamCommands
+{
+    public class AddQueamQueserCommand : IBotCommand
+    {
+        public Result CanExecute(CommandArgumentContainer args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Result<string>> ExecuteAsync(CommandArgumentContainer args)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string CommandName { get; } = "add-queam-queser";
+        public string Description { get; }
+        public string[] Args { get; } = {"Queam name, Queser id"};
+    }
+}
